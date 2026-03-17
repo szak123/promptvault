e2e92e4ef712bb3cd3dac16fdd85d1adad38464d
szak123 <sophia.a.zak@gmail.com>
2026-03-16 22:18:50 -0400

    Create page.tsx

---
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from '@clerk/nextjs'
+
+export default function SignUpPage() {
+  return (
+    <main style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', background: '#f5f0e8' }}>
+      <SignUp />
+    </main>
+  )
+}
